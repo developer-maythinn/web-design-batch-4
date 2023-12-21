9890ca89d51f31f535efb515580d447d60309d04
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2023-12-21 22:05:30 +0630

    day1-js

---
+++ JS/day1-js/script.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerText = "Hello World!";
+document.getElementById("demo").innerHTML = "<h1>Hello World!</h1>";
+document.getElementById("demo").style.background = "red";
