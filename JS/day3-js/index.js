0b776805d7de1ba5e17a53650c7af62c722d04c7
maythinn <90564038+developer-maythinn@users.noreply.github.com>
2023-12-28 21:54:46 +0630

    day3-js

---
+++ JS/day3-js/index.js
@@ -0,0 +1,7 @@
+let fruit = null;
+fruit = "apple";
+console.log(fruit);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(null == undefined);
+console.log(null === undefined);
